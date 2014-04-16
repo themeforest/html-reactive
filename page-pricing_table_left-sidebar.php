e18328ebc8eaca522da152881199bfbfbee382f5
bvcxz <sancak_2008@hotmail.com>
2014-04-16 13:24:45 +0300

    pricing table full width completed

--- page-pricing_table_left-sidebar.php
+++ page-pricing_table_left-sidebar.php
@@ -13,6 +13,7 @@
 </section> <!-- /.page-header -->
 
 <style type="text/css">
+
 .price-table {
     width: 100%;
 }
@@ -55,6 +56,20 @@
     margin: 0px;
     margin-top: -15px;
     position: relative;
+}
+
+.pricing-header-blur {
+    background: url('img/post-3.jpg');
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+-webkit-filter: blur(5px);
+  -moz-filter: blur(5px);
+  -o-filter: blur(5px);
+  -ms-filter: blur(5px);
+  filter: blur(5px);
 }
 
 /*list style 1*/
@@ -160,109 +175,26 @@
     text-align: center;
 }
 
-.pricing-header-blur {
-    background: url('img/post-3.jpg');
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 100%;
-    height: 100%;
--webkit-filter: blur(5px);
-  -moz-filter: blur(5px);
-  -o-filter: blur(5px);
-  -ms-filter: blur(5px);
-  filter: blur(5px);
-}
-
-.blurred-pricing-table {
-    background: url('img/footer-bg.jpg') no-repeat center center fixed;
-    padding: 100px 0px 130px 0px;
-    width: 100%;
-}
-
-/*pricing header transparent*/
-.price-table .pricing-header-style-transparent {
-    background-color: rgba(255,255,255,0.2);
-    color: #FFF;
-    height: 149px;
-    text-align: center;
-    position: relative;
-}
-
-.price-table .pricing-header-style-transparent h4 {
-    border-bottom: 1px solid #FFF;
-    font-size: 20px;
-    font-weight: 300;
-    padding: 10px 0px;
-    width: 75%;
-    position: relative;
-    margin: 0px auto;
-}
-
-.price-table .pricing-header-style-transparent p.pricing-value {
-    font-size: 54px;
-    margin-top: 0px;
-    position: relative;
-}
-
-.price-table .pricing-header-style-transparent p.pricing-value .pricing-currency {
-    font-size: 16px !important;
-    position: absolute;
-    top: 20px;
-    left: 40%;
-}
-
-.price-table .pricing-header-style-transparent p.pricing-duration {
-    font-size: 16px;
-    font-weight: 300;
-    margin: 0px;
-    margin-top: -15px;
-    position: relative;
-}
-
-/*list style transparent*/
-.price-table ul.pricing-ul-style-transparent {
-    padding: 0px;
-    width: 100%;
-}
-
-
-.price-table ul.pricing-ul-style-transparent li {
-    border-top: 1px solid #7d8c95;
-    color: #fff;
-    font-size: 14px;
-    font-weight: 300;
-    list-style: none;
-    text-align: center;
-    margin: 0px auto;
-    padding: 19px 0px;
-    width: 90%;
-}
-
-.price-table ul.pricing-ul-style-transparent li.pricing-first {
-    border-top: none;
-    padding: 34px 0px;
-}
-
-.price-table .pricing-buy-transparent {
-    background-color: rgba(255,255,255,0.2);
-    font-size: 16px;
-    width: 100%;
-    margin: -10px 0px 0px 0px;
-    padding: 20px 0px;
-    float: left;
-    color: #FFF;
-    text-align: center;
-}
-
-
 </style>
 
 
 <section class="container">
 	<div class="row">
-    	
-        <div class="col-md-12">
+    	<div class="col-md-3">
+        	<ul class="page-menu">
+            	<li><a href="page-about_us.php">About Us Page</a></li>
+                <li><a href="">Who We Are?</a></li>
+                <li><a href="">Our Clients</a></li>
+                <li><a href="">Meet the Team</a></li>
+                <li><a href="">What We Did?</a></li>
+                <li><a href="">Contact Page</a></li>
+                <li class="active"><a href="page-pricing_table.php">Pricing <i class="fa fa-angle-right pull-right"></i></a></li>
+                <li><a href="">Help</a></li>
+                <li><a href="">FAQ</a></li>
+                <li><a href="">Our Services</a></li>
+            </ul>
+        </div> <!-- /.col-md-3 -->
+        <div class="col-md-9">
         	
             <div class="content-line">
             	<h4><span class="active"><i class="fa fa-table"></i> Pricing Tables</h4>
@@ -403,164 +335,6 @@
                     </div> <!-- /.price-table-2 -->
                 </div> <!-- /.co-md-4 -->
             </div><!-- /.row -->
-
-            <div class="content-line">
-                <h4><span class="active"><i class="fa fa-table"></i> Pricing Tables Style 3</h4>
-                <div class="triangle"></div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="price-table">
-                        <div class="pricing-header-style">
-                        <div class="pricing-header-blur"></div>
-                            <h4>Bronze Pack</h4>
-                            <p class="pricing-value"><span class="pricing-currency">$</span> 39</p>
-                            <p class="pricing-duration">/month</p>
-                        </div>
-                        <ul class="pricing-ul-style-3">
-                            <li class="pricing-first">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</li>
-                            <li class="colored">Lorem ipsum dolor sit amet, consectetuer</li>
-                            <li>Lorem ipsum dolor sit amet</li>
-                            <li class="colored">Lorem ipsum dolor sit</li>
-                            <li>Lorem ipsum dolor sit amet lorem ipsum</li>
-                            <li class="colored">Lorem ipsum dolor sit amet lorem ipsum</li>
-                            <li>Lorem ipsum dolor  ipsum</li>
-                        </ul>
-                        <a href="" class="pricing-buy">BUY NOW</a>
-                    </div> <!-- /.price-table-3 -->
-                </div> <!-- /.co-md-4 -->
-
-
-                <div class="col-md-4">
-                    <div class="price-table">
-                        <div class="pricing-header-style">
-                        <div class="pricing-header-blur"></div>
-                            <h4>Gold Pack</h4>
-                            <p class="pricing-value"><span class="pricing-currency">$</span> 69</p>
-                            <p class="pricing-duration">/month</p>
-                        </div>
-                        <ul class="pricing-ul-style-3">
-                            <li class="pricing-first">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</li>
-                            <li class="colored">Lorem ipsum dolor sit amet, consectetuer</li>
-                            <li>Lorem ipsum dolor sit amet</li>
-                            <li class="colored">Lorem ipsum dolor sit</li>
-                            <li>Lorem ipsum dolor sit amet lorem ipsum</li>
-                            <li class="colored">Lorem ipsum dolor sit amet lorem ipsum</li>
-                            <li>Lorem ipsum dolor  ipsum</li>
-                        </ul>
-                        <a href="" class="pricing-buy">BUY NOW</a>
-                    </div> <!-- /.price-table-3 -->
-                </div> <!-- /.co-md-4 -->
-
-                <div class="col-md-4">
-                    <div class="price-table">
-                        <div class="pricing-header-style">
-                        <div class="pricing-header-blur"></div>
-                            <h4>Platinum Pack</h4>
-                            <p class="pricing-value"><span class="pricing-currency">$</span> 99</p>
-                            <p class="pricing-duration">/month</p>
-                        </div>
-                        <ul class="pricing-ul-style-3">
-                            <li class="pricing-first">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</li>
-                            <li class="colored">Lorem ipsum dolor sit amet, consectetuer</li>
-                            <li>Lorem ipsum dolor sit amet</li>
-                            <li class="colored">Lorem ipsum dolor sit</li>
-                            <li>Lorem ipsum dolor sit amet lorem ipsum</li>
-                            <li class="colored">Lorem ipsum dolor sit amet lorem ipsum</li>
-                            <li>Lorem ipsum dolor  ipsum</li>
-                        </ul>
-                        <a href="" class="pricing-buy">BUY NOW</a>
-                    </div> <!-- /.price-table-3 -->
-                </div> <!-- /.co-md-4 -->
-
-            </div><!-- /.row -->
-
-                
-            </div> <!-- /.content -->
-            <div class="h120"></div>
-
-        </div> <!-- /.co-md-9 -->
-    </div> <!-- /.row -->
-</section> <!-- /.container -->
-
-<div class="blurred-pricing-table">
-<section class="container">
-    <div class="row">
-
-        <div class="col-md-4">
-            <div class="price-table">
-                <div class="pricing-header-style-transparent">
-                    <h4>Bronz Pack</h4>
-                    <p class="pricing-value"><span class="pricing-currency">$</span> 39</p>
-                    <p class="pricing-duration">/month</p>
-                </div>
-                <ul class="pricing-ul-style-transparent">
-                    <li class="pricing-first">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</li>
-                    <li class="colored">Lorem ipsum dolor sit amet, consectetuer</li>
-                    <li>Lorem ipsum dolor sit amet</li>
-                    <li class="colored">Lorem ipsum dolor sit</li>
-                    <li>Lorem ipsum dolor sit amet lorem ipsum</li>
-                    <li class="colored">Lorem ipsum dolor sit amet lorem ipsum</li>
-                    <li>Lorem ipsum dolor  ipsum</li>
-                </ul>
-                <a href="" class="pricing-buy-transparent">BUY NOW</a>
-            </div> <!-- /.price-table-3 -->
-        </div> <!-- /.co-md-4 -->
-
-        <div class="col-md-4">
-            <div class="price-table">
-                <div class="pricing-header-style-transparent">
-                    <h4>Gold Pack</h4>
-                    <p class="pricing-value"><span class="pricing-currency">$</span> 69</p>
-                    <p class="pricing-duration">/month</p>
-                </div>
-                <ul class="pricing-ul-style-transparent">
-                    <li class="pricing-first">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</li>
-                    <li class="colored">Lorem ipsum dolor sit amet, consectetuer</li>
-                    <li>Lorem ipsum dolor sit amet</li>
-                    <li class="colored">Lorem ipsum dolor sit</li>
-                    <li>Lorem ipsum dolor sit amet lorem ipsum</li>
-                    <li class="colored">Lorem ipsum dolor sit amet lorem ipsum</li>
-                    <li>Lorem ipsum dolor  ipsum</li>
-                </ul>
-                <a href="" class="pricing-buy-transparent">BUY NOW</a>
-            </div> <!-- /.price-table-3 -->
-        </div> <!-- /.co-md-4 -->
-
-        <div class="col-md-4">
-            <div class="price-table">
-                <div class="pricing-header-style-transparent">
-                    <h4>Platinum Pack</h4>
-                    <p class="pricing-value"><span class="pricing-currency">$</span> 99</p>
-                    <p class="pricing-duration">/month</p>
-                </div>
-                <ul class="pricing-ul-style-transparent">
-                    <li class="pricing-first">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</li>
-                    <li class="colored">Lorem ipsum dolor sit amet, consectetuer</li>
-                    <li>Lorem ipsum dolor sit amet</li>
-                    <li class="colored">Lorem ipsum dolor sit</li>
-                    <li>Lorem ipsum dolor sit amet lorem ipsum</li>
-                    <li class="colored">Lorem ipsum dolor sit amet lorem ipsum</li>
-                    <li>Lorem ipsum dolor  ipsum</li>
-                </ul>
-                <a href="" class="pricing-buy-transparent">BUY NOW</a>
-            </div> <!-- /.price-table-3 -->
-        </div> <!-- /.co-md-4 --> 
-
-    </div><!-- /.row -->
-</section><!-- /.container -->
-</div> <!-- /.blurred-pricing-table -->
-
-
-
-<section class="container">
-    <div class="row">
-        
-        <div class="col-md-12">
-            
-
-            <div class="page-content">
 
             <div class="content-line">
                 <h4><span class="active"><i class="fa fa-table"></i> Pricing Tables Style 3</h4>
