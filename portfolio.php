0e6b03e0467e546973fbe6d5fb317ebec02c401c
bvcxz <sancak_2008@hotmail.com>
2014-04-17 13:46:45 +0300

    portfolio

---
+++ portfolio.php
@@ -0,0 +1,200 @@
+<?php include('header.php'); ?>
+
+
+<section class="page-header">
+<div class="opacity-black">
+    <div class="container">
+        <h1 class="title">PORTFOLIO</h1>
+        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+        <div class="navi">
+            You are here : <a href="index.html">Home</a>  > <a href="page.html">Portfolio</a>
+        
+        </div>
+    </div> <!-- /.container -->
+</div> <!-- /.opacity-black -->
+</section> <!-- /.page-header -->
+
+
+<div class="h100"></div>
+
+<section class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+
+        <div class="content-header js-hide-me">
+        <h2>OUR PORTFOLIO</h2>
+        <p>Lorem ipsum dolor smit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod <br />tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+    </div> <!-- /.content-header -->
+    <div class="h80"></div>
+    
+    
+    <div class="text-center js-hide-me">
+        <a class="btn btn-default btn-filter selected" data-tag="">All</a>
+     
+        <a class="btn btn-default btn-filter" data-tag="photoshop">Photoshop</a>
+   
+        <a class="btn btn-default btn-filter" data-tag="css">Css</a>
+ 
+        <a class="btn btn-default btn-filter" data-tag="jquery">jQuery</a>
+
+        <a class="btn btn-default btn-filter" data-tag="php">Php</a>
+    </div>
+    <div class="h40"></div>
+    
+    
+    
+    
+    <div class="row">
+        <div class="col-md-3 col-xs-6 js-hide-me tag-box tag-php ">
+            <div class="portfolio image-hover img-blur-2">
+                <div class="opacity op-20 left" rel="tag-php">
+                    <div class="buttons">
+                        <div class="buttons_in">
+                            <a href="img/team-3.jpg" class="btn btn-theme-opacity btn-sm fancybox" title="JHON DOE &raquo; Grapchic Designer"><i class="fa fa-expand"></i> ZOOM</a>
+                            <a href="" class="btn btn-theme-opacity btn-sm"><i class="fa fa-eye"></i> VIEW</a>
+                        </div> <!-- /.buttons_in -->
+                    </div> <!-- /.buttons -->
+                </div> <!-- /.opacity -->
+                <img src="img/portfolio/3.jpg" class="team-image img-responsive" alt="image title">
+            </div>
+            <span class="portfolio-description">Retro Style Camera</span>
+        </div> <!-- /.col-md-3 -->
+        <div class="col-md-3 col-xs-6 js-hide-me tag-box tag-css">
+            <div class="portfolio image-hover img-blur-2">
+                <div class="opacity op-20 left" rel="tag-css">
+                    <div class="buttons">
+                        <div class="buttons_in">
+                            <a href="img/team-3.jpg" class="btn btn-theme-opacity btn-sm fancybox" title="JHON DOE &raquo; Grapchic Designer"><i class="fa fa-expand"></i> ZOOM</a>
+                            <a href="" class="btn btn-theme-opacity btn-sm"><i class="fa fa-eye"></i> VIEW</a>
+                        </div> <!-- /.buttons_in -->
+                    </div> <!-- /.buttons -->
+                </div> <!-- /.opacity -->
+                <img src="img/portfolio/3.jpg" class="team-image img-responsive" alt="image title">
+            </div>
+            <span class="portfolio-description">Retro Style Camera</span>
+        </div> <!-- /.col-md-3 -->
+        <div class="col-md-3 col-xs-6 js-hide-me tag-box tag-php">
+            <div class="portfolio image-hover img-blur-2">
+                <div class="opacity op-20 left" rel="tag-php">
+                    <div class="buttons">
+                        <div class="buttons_in">
+                            <a href="img/team-3.jpg" class="btn btn-theme-opacity btn-sm fancybox" title="JHON DOE &raquo; Grapchic Designer"><i class="fa fa-expand"></i> ZOOM</a>
+                            <a href="" class="btn btn-theme-opacity btn-sm"><i class="fa fa-eye"></i> VIEW</a>
+                        </div> <!-- /.buttons_in -->
+                    </div> <!-- /.buttons -->
+                </div> <!-- /.opacity -->
+                <img src="img/portfolio/3.jpg" class="team-image img-responsive" alt="image title">
+            </div>
+            <span class="portfolio-description">Retro Style Camera</span>
+        </div> <!-- /.col-md-3 -->
+        <div class="col-md-3 col-xs-6 js-hide-me tag-box tag-photoshop">
+            <div class="portfolio image-hover img-blur-2">
+                <div class="opacity op-20 left" rel="tag-photoshop">
+                    <div class="buttons">
+                        <div class="buttons_in">
+                            <a href="img/team-3.jpg" class="btn btn-theme-opacity btn-sm fancybox" title="JHON DOE &raquo; Grapchic Designer"><i class="fa fa-expand"></i> ZOOM</a>
+                            <a href="" class="btn btn-theme-opacity btn-sm"><i class="fa fa-eye"></i> VIEW</a>
+                        </div> <!-- /.buttons_in -->
+                    </div> <!-- /.buttons -->
+                </div> <!-- /.opacity -->
+                <img src="img/portfolio/3.jpg" class="team-image img-responsive" alt="image title">
+            </div>
+            <span class="portfolio-description">Retro Style Camera</span>
+        </div> <!-- /.col-md-3 -->
+        <div class="col-md-3 col-xs-6 js-hide-me tag-box tag-css">
+            <div class="portfolio image-hover img-blur-2">
+                <div class="opacity op-20 left" rel="tag-css">
+                    <div class="buttons">
+                        <div class="buttons_in">
+                            <a href="img/team-3.jpg" class="btn btn-theme-opacity btn-sm fancybox" title="JHON DOE &raquo; Grapchic Designer"><i class="fa fa-expand"></i> ZOOM</a>
+                            <a href="" class="btn btn-theme-opacity btn-sm"><i class="fa fa-eye"></i> VIEW</a>
+                        </div> <!-- /.buttons_in -->
+                    </div> <!-- /.buttons -->
+                </div> <!-- /.opacity -->
+                <img src="img/portfolio/3.jpg" class="team-image img-responsive" alt="image title">
+            </div>
+            <span class="portfolio-description">Retro Style Camera</span>
+        </div> <!-- /.col-md-3 -->
+        <div class="col-md-3 col-xs-6 js-hide-me tag-box tag-php">
+            <div class="portfolio image-hover img-blur-2">
+                <div class="opacity op-20 left" rel="tag-php">
+                    <div class="buttons">
+                        <div class="buttons_in">
+                            <a href="img/team-3.jpg" class="btn btn-theme-opacity btn-sm fancybox" title="JHON DOE &raquo; Grapchic Designer"><i class="fa fa-expand"></i> ZOOM</a>
+                            <a href="" class="btn btn-theme-opacity btn-sm"><i class="fa fa-eye"></i> VIEW</a>
+                        </div> <!-- /.buttons_in -->
+                    </div> <!-- /.buttons -->
+                </div> <!-- /.opacity -->
+                <img src="img/portfolio/3.jpg" class="team-image img-responsive" alt="image title">
+            </div>
+            <span class="portfolio-description">Retro Style Camera</span>
+        </div> <!-- /.col-md-3 -->
+        <div class="col-md-3 col-xs-6 js-hide-me tag-box tag-css">
+            <div class="portfolio image-hover img-blur-2">
+                <div class="opacity op-20 left" rel="tag-php">
+                    <div class="buttons">
+                        <div class="buttons_in">
+                            <a href="img/team-3.jpg" class="btn btn-theme-opacity btn-sm fancybox" title="JHON DOE &raquo; Grapchic Designer"><i class="fa fa-expand"></i> ZOOM</a>
+                            <a href="" class="btn btn-theme-opacity btn-sm"><i class="fa fa-eye"></i> VIEW</a>
+                        </div> <!-- /.buttons_in -->
+                    </div> <!-- /.buttons -->
+                </div> <!-- /.opacity -->
+                <img src="img/portfolio/3.jpg" class="team-image img-responsive" alt="image title">
+            </div>
+            <span class="portfolio-description">Retro Style Camera</span>
+        </div> <!-- /.col-md-3 -->
+        <div class="col-md-3 col-xs-6 js-hide-me tag-box tag-photoshop">
+            <div class="portfolio image-hover img-blur-2">
+                <div class="opacity op-20 left" rel="tag-php">
+                    <div class="buttons">
+                        <div class="buttons_in">
+                            <a href="img/team-3.jpg" class="btn btn-theme-opacity btn-sm fancybox" title="JHON DOE &raquo; Grapchic Designer"><i class="fa fa-expand"></i> ZOOM</a>
+                            <a href="" class="btn btn-theme-opacity btn-sm"><i class="fa fa-eye"></i> VIEW</a>
+                        </div> <!-- /.buttons_in -->
+                    </div> <!-- /.buttons -->
+                </div> <!-- /.opacity -->
+                <img src="img/portfolio/3.jpg" class="team-image img-responsive" alt="image title">
+            </div>
+            <span class="portfolio-description">Retro Style Camera</span>
+        </div> <!-- /.col-md-3 -->
+    </div> <!-- /.row -->
+
+    <script>
+    /* portfolio */
+    $('.btn-filter').click(function() {
+        $('.btn-filter').removeClass('selected');
+        $(this).addClass('selected');
+        
+        var tag = $(this).data('tag');
+        var box = $('.tag-box');
+        box.stop(true, true)
+        .animate({
+        opacity: '0'},{
+        duration: 300,
+        complete: function(){
+            box.hide();
+            $('.tag-'+tag).show();
+            $('.tag-'+tag).animate({opacity: '1'}, {
+                duration: 300
+            });
+            
+            if(tag == '')
+            {
+                box.show();
+                box.animate({opacity: '1'}, {
+                    duration: 300
+                });
+            }
+        }});
+    });
+    </script>
+
+
+
+        </div> <!-- /.co-md-12 -->
+    </div> <!-- /.row -->
+</section> <!-- /.container -->
+
+    
+<?php include('footer.php'); ?>
